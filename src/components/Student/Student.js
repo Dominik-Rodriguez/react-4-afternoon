5dc6a090aec66cf228c46a91e8cfa986055eadf9
dominikrodriguez <joeangdom@gmail.com>
2020-10-01 17:55:54 -0600

    working on the black diamond problem

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
 
+    this.state = {
+      studentInfo: {}
+    };
+  }
+
+  componentDidMount() {
+    return axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(results => {
+        this.setState({
+          studentInfo: results.data
+        });
+      });
   }
 
   render() {
+    const {studentInfo} = this.state
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>Student:</h1>
+        <h1>
+          {studentInfo.first_name} {studentInfo.last_name}
+        </h1>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
+        <Link to={`/classlist/`}><button className="backbtn">Back</button></Link> 
+        {/* <button classname='backbtn' onClick={() => history.goBack()}>Back</button> */}
       </div>
-    )
+    );
   }
 }
